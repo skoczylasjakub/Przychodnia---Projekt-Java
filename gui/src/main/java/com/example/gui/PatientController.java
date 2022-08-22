ddcf0de6f3ff77b6715bbeb7a3722136de766652
skoczylasjakub <92790525+skoczylasjakub@users.noreply.github.com>
2022-08-23 00:27:06 +0200

    Add files via upload
    switch stages etc.

---
+++ gui/src/main/java/com/example/gui/PatientController.java
@@ -0,0 +1,33 @@
+package com.example.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class PatientController {
+
+    @FXML
+    private Button exitApp;
+
+    @FXML
+    private Button myPrescription;
+
+    @FXML
+    private Button myVisits;
+
+    @FXML
+    void onExitAppClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMyPrescriptionAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMyVisitsClick(ActionEvent event) {
+
+    }
+
+}
